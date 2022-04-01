bbfa1d257f9ad0ee15e71e2c3870b45427ce95bc
rogowskid <rogowskid11@gmail.com>
2022-04-01 18:30:43 +0200

    add redirect after login

--- src/main/java/pl/ticketsystem/ticketsystem/Account/AccountController.java
+++ src/main/java/pl/ticketsystem/ticketsystem/Account/AccountController.java
@@ -1,7 +1,17 @@
 package pl.ticketsystem.ticketsystem.Account;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class AccountController {
+
+    @GetMapping("/index")
+    public String index()
+    {
+
+        return "Hello world!";
+    }
 }
--- src/main/java/pl/ticketsystem/ticketsystem/Config/LoginConfiguration.java
+++ src/main/java/pl/ticketsystem/ticketsystem/Config/LoginConfiguration.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
@@ -15,6 +18,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class LoginConfiguration extends WebSecurityConfigurerAdapter {
 
     private AccountDetalisService accountDetalisService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().defaultSuccessUrl("/index", true)
+
+
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+
+
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
