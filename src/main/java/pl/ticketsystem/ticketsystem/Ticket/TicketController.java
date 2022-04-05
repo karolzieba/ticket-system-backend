886d208898d315af7b054236f2fae6acffd89b8f
Karol Zięba <kz516749@gmail.com>
2022-04-06 00:24:34 +0200

    added api for tickets

--- src/main/java/pl/ticketsystem/ticketsystem/Ticket/TicketController.java
+++ src/main/java/pl/ticketsystem/ticketsystem/Ticket/TicketController.java
@@ -1,7 +1,41 @@
 package pl.ticketsystem.ticketsystem.Ticket;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(path="api/ticket")
 public class TicketController {
+    private final TicketService ticketService;
+
+    @Autowired
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @GetMapping
+    public List<Ticket> getTickets() {
+        return ticketService.getTickets();
+    }
+
+    @GetMapping(path="{ticketId}")
+    public Ticket getTicket(@PathVariable long ticketId) {
+        return ticketService.getTicket(ticketId).get();
+    }
+
+    @PostMapping
+    public void addTicket(@RequestBody Ticket ticket) {
+        ticketService.addTicket(ticket);
+    }
+
+    @PutMapping(path="{ticketId}")
+    public void updateTicket(@PathVariable long ticketId, @RequestBody Ticket ticket) {
+        ticketService.updateTicket(ticketId, ticket);
+    }
+
+    @DeleteMapping(path="{ticketId}")
+    public void deleteTicket(@PathVariable long ticketId) { ticketService.deleteTicket(ticketId); }
 }
--- src/main/java/pl/ticketsystem/ticketsystem/Ticket/TicketService.java
+++ src/main/java/pl/ticketsystem/ticketsystem/Ticket/TicketService.java
@@ -1,7 +1,46 @@
 package pl.ticketsystem.ticketsystem.Ticket;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
-@Controller
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
 public class TicketService {
+    private TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    public List<Ticket> getTickets() {
+        return ticketRepository.findAll();
+    }
+    public Optional<Ticket> getTicket(long id) {
+        return ticketRepository.findById(id);
+    }
+    public void addTicket(Ticket ticket) {
+        if(!Objects.isNull(ticket.getDateTicketBuy())) {
+            ticketRepository.save(ticket);
+        }
+    }
+
+    public void updateTicket(long id, Ticket ticket) {
+        if(ticketRepository.existsById(id)) {
+            Ticket updatedTicket = ticketRepository.findById(id).orElseThrow(() -> new IllegalStateException("Ticket with this ID does not exist!"));
+
+            if(!Objects.isNull(ticket.getClient())) {
+                updatedTicket.setClient(ticket.getClient());
+            }
+
+            ticketRepository.save(updatedTicket);
+        }
+        else {
+            throw new IllegalStateException("Ticket with this ID does not exist!");
+        }
+    }
+    public void deleteTicket(long id) {
+        ticketRepository.deleteById(id);
+    }
 }
