b701725d06c1a7ed65e8b61130b19b923fbc4888
PatrykDuda <p432@interia.pl>
2022-05-24 13:11:36 +0200

    IT

--- src/test/java/pl/ticketsystem/ticketsystem/Event/EventControllerIntegrationTest.java
+++ src/test/java/pl/ticketsystem/ticketsystem/Event/EventControllerIntegrationTest.java
@@ -9,6 +9,8 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -17,6 +19,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(value = "/insert_data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clean_database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@WithMockUser(username="moderator",roles={"MODERATOR"})
 class EventControllerIntegrationTest {
     @LocalServerPort
     private int serverPort;
@@ -34,6 +39,43 @@ class EventControllerIntegrationTest {
         // when:
         RequestEntity<Void> request = RequestEntity
                 .get(createServerAddress())
+                .build();
+        ResponseEntity<List<Event>> response = restTemplate.exchange(request, new ParameterizedTypeReference<List<Event>>(){});
+        // then:
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void shouldReturnClientOrders() throws Exception {
+        // when:
+        RequestEntity<Void> request = RequestEntity
+                .get(createServerAddress() + "/client/orders/1")
+                .build();
+        ResponseEntity<List<Object[]>> response = restTemplate.exchange(request, new ParameterizedTypeReference<List<Object[]>>(){});
+        // then:
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void shouldNotReturnEvent() throws Exception {
+        // when:
+        RequestEntity<Void> request = RequestEntity
+                .get(createServerAddress() + "/1")
+                .build();
+        ResponseEntity<Event> response = restTemplate.exchange(request, new ParameterizedTypeReference<Event>(){});
+        // then:
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void shouldReturnCategoryEvent() throws Exception {
+        // when:
+        RequestEntity<Void> request = RequestEntity
+                .get(createServerAddress() + "/category/1")
                 .build();
         ResponseEntity<List<Event>> response = restTemplate.exchange(request, new ParameterizedTypeReference<List<Event>>(){});
         // then:
